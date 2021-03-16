2fa0dce88e1487680144dd8dc36d527ac2001bb1
Srinivasa Rao Thandra <srinivasa.thandra@nike.com>
2021-03-15 22:41:28 -0700

    Added new changes

--- src/main/java/org/functionalrunner/FP01Functional.java
+++ src/main/java/org/functionalrunner/FP01Functional.java
@@ -8,8 +8,8 @@ public class FP01Functional {
         long startTime = 0L;
         long endTime = 0L;
         startTime = System.currentTimeMillis();
-        List<Integer> numbers = List.of(12, 13, 4, 6, 2);
-        printEvenNumbersInListFunctional(numbers);
+        List<Integer> numbers = List.of(12, 13, 4, 6, 7, 2);
+        printSquaresOfEvenNumbersInListFunctional(numbers);
         endTime = System.currentTimeMillis();
 
         System.out.println("Time took " + (endTime - startTime));
@@ -19,25 +19,30 @@ public class FP01Functional {
         System.out.println("number = " + number);
     }
 
-    private static boolean isEven(int number){
-        return number%2==0;
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-/*        for (int number : numbers) {
-            System.out.println("number = " + number);
-        }*/
+
         numbers.stream().forEach(System.out::println);
 
     }
 
 
     private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
-/*        for (int number : numbers) {
-            System.out.println("number = " + number);
-        }*/
+
         numbers.stream().
                 filter(FP01Functional::isEven).
+                forEach(System.out::println);
+
+    }
+
+    private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+
+        numbers.stream().
+                filter(FP01Functional::isEven).
+                map(number -> number * number).
                 forEach(System.out::println);
 
     }
