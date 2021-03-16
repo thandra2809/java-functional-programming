dab99e9d0d9a5caad220902c502f34bf3c2fa977
Srinivasa Rao Thandra <srinivasa.thandra@nike.com>
2021-03-15 21:14:26 -0700

    Added new changes

---
+++ src/main/java/org/functionalrunner/FunctionalRunner.java
@@ -0,0 +1,9 @@
+package org.functionalrunner;
+
+
+public class FunctionalRunner {
+    public static void main(String[] args) {
+        System.out.println("Welcome to functional Programming");
+    }
+
+}
